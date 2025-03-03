041802011c80adaba03410082df6bd4edcb8ca08
Alefe Rocha <alefe.rtavares@gmail.com>
2025-03-03 10:32:07 -0300

    timing interceptor created

---
+++ src/common/interceptor/timing-connection.interceptor.ts
@@ -0,0 +1,16 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { tap } from "rxjs";
+
+export class TimingConnectionInterceptor implements NestInterceptor {
+    async intercept(context: ExecutionContext, next: CallHandler<any>) {
+        const startTime = Date.now()
+
+        return next.handle().pipe(
+            tap(() => {
+                const finalTime = Date.now()
+                const elapsedTime = finalTime - startTime
+                console.log(`This call expends ${elapsedTime}ms.`)
+            })
+        )
+    }
+}
